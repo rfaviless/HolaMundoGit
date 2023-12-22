277b85eaadd4e50d575b7434bcf8d87d4fa05ba6
rfaviless <rfaviless@gmail.com>
2023-12-22 14:46:26 +0100

    2o commit, 1er cambio

--- src/Principal/HolaMundo2.java
+++ src/Principal/HolaMundo2.java
@@ -5,6 +5,7 @@ public class HolaMundo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo!!");
+		System.out.println("1er cambio");
 	}
 
 }
