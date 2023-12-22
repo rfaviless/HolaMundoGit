1099617c2934b82c0c78a6c30d8eac6a3bc0b76a
rfaviless <rfaviless@gmail.com>
2023-12-22 22:08:24 +0100

    3er cambio, version 4

--- src/Principal/HolaMundo2.java
+++ src/Principal/HolaMundo2.java
@@ -4,9 +4,9 @@ public class HolaMundo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo!!");
-		System.out.println("1er cambio");
-		System.out.println("2o cambio");
+		System.out.println("Hola mundo!! Version");
+		System.out.println("1er cambio, version 2");
+		System.out.println("2o cambio, version 3");
 	}
 
 }
